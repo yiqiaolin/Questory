0379836a9d637327fc996fb982f6f6e12469ea96
yiqiaolin <yiqiaolin017@gmail.com>
2026-07-08 18:12:29 +0800

    refactor: rename room file to room_prepare

--- js/main.js
+++ js/main.js
@@ -131,7 +131,7 @@ itemArea.addEventListener("click", function (e) {
     const item = e.target.closest(".item");
     if (!item) return;
     const roomId = item.dataset.id;
-    window.location.href = `room.html?id=${roomId}`;
+    window.location.href = `room_prepare.html?id=${roomId}`;
 });
 
 // 點擊執行申請加入副本動作
